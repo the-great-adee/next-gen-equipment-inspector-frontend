b9742b4e18ee5a281d24c91600ea3facd9afaea8
Aditya Gupta <adityagupta46772@gmail.com>
2024-08-09 17:33:08 +0530

    Created login page.

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import New from './pages/New';
 import Records from './pages/Records';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Login from './pages/Login';
 
 
 
 const App = () => {
   return (
-    <div className='min-h-full h-fit overflow-y-scroll pt-20 overflow-hidden'>
+    <div className=''>
       <HeadNavBar/>
-      <div className="">
+      <div className="min-h-full relative h-fit overflow-y-scroll pt-20 overflow-hidden">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/records" element={<Records />} />
           <Route path="/new" element={<New />} />
           <Route path="/contact" element={<Contact/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
         <Footer/>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,8 @@ const Home = () => {
     <div className="px-6 h-full space-y-4">
         <TagLine/>
         <div className="font-mono text-xl">Login Here to view your Records</div>
-        <div className=" bg-black rounded-lg w-28 text-center px-4 pb-3 p-2 text-white">
-        <Link  to={"/login"}>Login</Link>
-        
+        <div >
+        <Link  to={"/login"} className=" bg-black rounded-lg w-28 text-center px-6 pb-3 p-2 text-white">Login</Link>
         </div>
         
     </div>
